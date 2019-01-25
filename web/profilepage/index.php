f98cb7197a8364c3613a758dd4661f1c902533b8
ankitadev <ankitae.dev@gmail.com>
2019-01-24 19:39:59 -0700

    visited feature

--- web/profilepage/index.php
+++ web/profilepage/index.php
@@ -17,6 +17,33 @@
 <?php
 echo "Today is " . date("Y/m/d") . " " . date("l") ."<br>";
 ?>
+<?php
+if(isset($_COOKIE['AboutVisit']))
+{
+$last = $_COOKIE['AboutVisit']; }
+$year = 31536000 + time() ;
+//this adds one year to the current time, for the cookie expiration
+setcookie(AboutVisit, time (), $year) ;
+if (isset ($last))
+{
+$change = time () - $last;
+if ( $change > 86400)
+{
+echo "Welcome back! <br> You last visited on ". date("m/d/y",$last) ;
+// Tells the user when they last visited if it was over a day ago
+}
+else
+{
+echo "Thanks for using our site!";
+//Gives the user a message if they are visiting again in the same day
+}
+}
+else
+{
+echo "Welcome to our site!";
+//Greets a first time user
+}
+?> 
 <div class="topnav">
   <a class="active" href="https://thawing-mountain-32172.herokuapp.com/profilepage/index.php">Home</a>
   <a href="https://thawing-mountain-32172.herokuapp.com/profilepage/index.php">Contact</a>
