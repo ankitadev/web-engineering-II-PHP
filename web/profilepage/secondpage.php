bf0778c912274b0bf987275b54ec58590fe3f8d1
ankitadev <ankitae.dev@gmail.com>
2019-01-24 19:42:56 -0700

    visited feature

--- web/profilepage/secondpage.php
+++ web/profilepage/secondpage.php
@@ -16,7 +16,6 @@ if(isset($_COOKIE['AboutVisit']))
 {
 $last = $_COOKIE['AboutVisit']; }
 $year = 31536000 + time() ;
-//this adds one year to the current time, for the cookie expiration
 setcookie(AboutVisit, time (), $year) ;
 if (isset ($last))
 {
@@ -24,18 +23,15 @@ $change = time () - $last;
 if ( $change > 86400)
 {
 echo "Welcome back! <br> You last visited on ". date("m/d/y",$last) ;
-// Tells the user when they last visited if it was over a day ago
 }
 else
 {
-echo "Thanks for using our site!";
-//Gives the user a message if they are visiting again in the same day
+echo "Thanks for visiting!";
 }
 }
 else
 {
-echo "Welcome to our site!";
-//Greets a first time user
+echo "Welcome to my site!";
 }
 ?> 
 
