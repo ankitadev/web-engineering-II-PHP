fe741dd4e54e26d4834470a42abe33a5a17a7998
ankitadev <ankitae.dev@gmail.com>
2019-01-26 16:09:39 -0700

    nav bar

--- web/shoppingcart/browse.php
+++ web/shoppingcart/browse.php
@@ -14,9 +14,22 @@
 <body>
 
 	<div class="container-fluid">
-		<div class="page-header">
-			<h1>Welcome shoppers</h1>
-		</div>
+			<nav class="navbar navbar-inverse navbar-fixed-top">
+			  <div class="container-fluid">
+			    <div class="navbar-header">
+			      <a class="navbar-brand" href="https://thawing-mountain-32172.herokuapp.com/shoppingcart/browse.php">Naruto Magaziiine</a>
+			    </div>
+			    <ul class="nav navbar-nav">
+			      <li class="active"><a href="https://thawing-mountain-32172.herokuapp.com/shoppingcart/browse.php">Home</a></li>
+			      <li><a href="#">Page 1</a></li>
+			      <li><a href="#">Page 2</a></li>
+			      <li><a href="#">Page 3</a></li>
+			    </ul>
+			 	<ul class="nav navbar-nav navbar-right">
+			      <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span>Cart</a></li>
+			    </ul>
+			  </div>
+			</nav>
 		<div class="row">
 			<div class="col-sm-4">
 				<div class="thumbnail">
