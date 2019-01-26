ea31675b2e84ad3268263b534317c36a15bcfabf
ankitadev <ankitae.dev@gmail.com>
2019-01-26 15:32:03 -0700

    shopping cart browse page

---
+++ web/shoppingcart/browser.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Browse shopping cart</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+	<div class="container-fluid">
+		<div class="page-header">
+			<h1>Welcome shoppers</h1>
+		</div>
+		<div class="row">
+			<div class="colsm-4"><img src="magazine1.jpeg"></div>
+			<div class="colsm-4"><img src="magazine2.jpeg"></div>
+			<div class="colsm-4"><img src="magazine3.jpeg"></div>
+		</div>
+	</div>
+	
+</body>
+</html>]
