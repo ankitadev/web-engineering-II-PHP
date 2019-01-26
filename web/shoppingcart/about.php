d8f3caef8951c379493956fac58f404397765a80
ankitadev <ankitae.dev@gmail.com>
2019-01-26 16:52:38 -0700

    added new php pages

---
+++ web/shoppingcart/about.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About US</title>
+	<meta charset="utf-8"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container-fluid">
+		<div class="page-header">
+			<nav class="navbar navbar-inverse navbar-fixed-top">
+			  <div class="container-fluid">
+			    <div class="navbar-header">
+			      Naruto Magaziiine
+			    </div>
+			    <ul class="nav navbar-nav">
+			      <li class="active"><a href="https://thawing-mountain-32172.herokuapp.com/shoppingcart/browse.php">Home</a></li>
+			      <li><a href="https://thawing-mountain-32172.herokuapp.com/shoppingcart/about.php">About Us</a></li>
+			    </ul>
+			 	<ul class="nav navbar-nav navbar-right">
+			      <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+			    </ul>
+			  </div>
+			</nav>
+		</div>
+		<div class="panel panel-default">
+	    <div class="panel-footer">Panel Footer</div>
+  	</div>
+</div>
+</body>
+</html>
--- web/shoppingcart/browse.php
+++ web/shoppingcart/browse.php
@@ -1,3 +1,7 @@
+<?php
+// Start the session
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,16 +22,14 @@
 			<nav class="navbar navbar-inverse navbar-fixed-top">
 			  <div class="container-fluid">
 			    <div class="navbar-header">
-			      <a class="navbar-brand" href="https://thawing-mountain-32172.herokuapp.com/shoppingcart/browse.php">Naruto Magaziiine</a>
+			      Naruto Magaziiine
 			    </div>
 			    <ul class="nav navbar-nav">
 			      <li class="active"><a href="https://thawing-mountain-32172.herokuapp.com/shoppingcart/browse.php">Home</a></li>
-			      <li><a href="#">Page 1</a></li>
-			      <li><a href="#">Page 2</a></li>
-			      <li><a href="#">Page 3</a></li>
+			      <li><a href="https://thawing-mountain-32172.herokuapp.com/shoppingcart/about.php">About Us</a></li>
 			    </ul>
 			 	<ul class="nav navbar-nav navbar-right">
-			      <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+			      <li><a href="https://thawing-mountain-32172.herokuapp.com/shoppingcart/about.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
 			    </ul>
 			  </div>
 			</nav>
@@ -37,7 +39,7 @@
 				<div class="thumbnail">
 				<img src="magazine1.jpeg" class="img-thumbnail" alt="Naruto Shippuden Summer 2009 Collector">
 				<div class="caption">
-           			 <p class="text-center"><small>Naruto Shippuden Summer 2009 Collector</small></p>
+           			 <p class="text-center text-muted">Naruto Shippuden Summer 2009 Collector</p>
            			 <button type="button" class="btn btn-primary btn-block">Add to Cart</button>
           		</div>
           	</div>
@@ -46,7 +48,7 @@
 				<div class="thumbnail">
 				<img src="magazine2.jpeg" class="img-thumbnail" alt="Naruto Winter 2007/2008 Collector">
 				<div class="caption">
-	           		 <p class="text-center"><small>Naruto Winter 2007/2008 Collector</small></p>
+	           		 <p class="text-center text-muted">Naruto Winter 2007/2008 Collector</p>
 	           		 <button type="button" class="btn btn-primary btn-block">Add to Cart</button>
 	         	</div>
 	         </div>
@@ -55,7 +57,7 @@
 				<div class="thumbnail">
 				<img src="magazine3.jpeg" class="img-thumbnail" alt="Naruto Collector 3">
 				<div class="caption">
-	            	<p class="text-center"><small>Naruto Collector 3</small></p>
+	            	<p class="text-center text-muted">Naruto Collector 3</p>
 	            	<button type="button" class="btn btn-primary btn-block">Add to Cart</button>
 	         	 </div>
 	         	</div>
@@ -64,32 +66,35 @@
 		<div class="row">
 			<div class="col-sm-4">
 				<div class="thumbnail">
-				<img src="magazine1.jpeg" class="img-thumbnail" alt="Naruto Shippuden Summer 2009 Collector">
+				<img src="magazine4.jpeg" class="img-thumbnail" alt="Naruto Shippuden Summer 2009 Collector">
 				<div class="caption">
-           			 <p class="text-center"><small>Naruto Shippuden Summer 2009 Collector</small></p>
+           			 <p class="text-center text-muted">Naruto Shippuden Summer 2009 Collector</p>
            			 <button type="button" class="btn btn-primary btn-block">Add to Cart</button>
           		</div>
           	</div>
 			</div>
 			<div class="col-sm-4">
 				<div class="thumbnail">
-				<img src="magazine2.jpeg" class="img-thumbnail" alt="Naruto Winter 2007/2008 Collector">
+				<img src="magazine5.jpeg" class="img-thumbnail" alt="Naruto Winter 2007/2008 Collector">
 				<div class="caption">
-	           		 <p class="text-center"><small>Naruto Winter 2007/2008 Collector</small></p>
+	           		 <p class="text-center text-muted">Naruto Winter 2007/2008 Collector</p>
 	           		 <button type="button" class="btn btn-primary btn-block">Add to Cart</button>
 	         	</div>
 	         </div>
 			</div>
 			<div class="col-sm-4">
 				<div class="thumbnail">
-				<img src="magazine3.jpeg" class="img-thumbnail" alt="Naruto Collector 3">
+				<img src="magazine6.jpeg" class="img-thumbnail" alt="Naruto Collector 3">
 				<div class="caption">
-	            	<p class="text-center"><small>Naruto Collector 3</small></p>
+	            	<p class="text-center text-muted">Naruto Collector 3</p>
 	            	<button type="button" class="btn btn-primary btn-block">Add to Cart</button>
 	         	 </div>
 	         	</div>
 			</div>
 		</div>
+	<div class="panel panel-default">
+	    <div class="panel-footer">Panel Footer</div>
+  	</div>
 	</div>
 	
 </body>
