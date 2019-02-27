77b961d15646fab72c5a0bf6839b78affe05a5ca
ankitadev <ankitae.dev@gmail.com>
2019-02-27 12:15:56 -0700

    personal project1 test10

--- web/exercisegenerator/index.php
+++ web/exercisegenerator/index.php
@@ -34,15 +34,18 @@ else
 <div class="row">
   <div class="col-sm-4"><img style="width: 100%" src="background2.jpeg" alt="jpeg"></div>
   <div class="col-sm-8">
-  	<nav class="navbar navbar-inverse">
-  		<p class="navbar-text"><a class="navbar-brand" href="https://thawing-mountain-32172.herokuapp.com/exercisegenerator/index.php">Fitness Calculator</a></p>
-  		<ul class="nav navbar-nav navbar-right">
-			 	<li><a href="signOut.php"><span class="glyphicon glyphicon-user"></span> <?= $username ?></a></li>
-			      <li><a href="signOut.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
-			      
-			    </ul>
-	</nav>
-<h2 class= "signInH2">Answer questions below to find the best workout to help you reach your goals!</h2>
+	<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="https://thawing-mountain-32172.herokuapp.com/exercisegenerator/index.php">Fitness Calculator</a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span><?= $username ?></a></li>
+      <li><a href="signOut.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+    </ul>
+  </div>
+</nav>
+<h2 class= "signInH2">Answer questions below to find your progress!</h2>
   <form class="form-horizontal" id="mainForm" action="submit.php" method="POST">
     <div class="form-group">
       <label class="control-label col-sm-2" for="txtUser">Username: </label>
