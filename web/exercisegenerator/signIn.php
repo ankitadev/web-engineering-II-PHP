eed4d48da8d73662fcd850bc053d3a8bf9ec85be
ankitadev <ankitae.dev@gmail.com>
2019-02-27 11:06:27 -0700

    personal project1 test9

--- web/exercisegenerator/signIn.php
+++ web/exercisegenerator/signIn.php
@@ -76,32 +76,36 @@ if ($badLogin)
 }
 ?>
 
-<div class="page-header">
-			<nav class="navbar navbar-inverse">
-  
-  <p class="navbar-text">Welcome TO The Workout Calculator</p>
-</nav>
-		</div>
-
 <div class="row">
   <div class="col-sm-4"><img style="width: 100%" src="background.jpeg" alt="jpeg"></div>
   <div class="col-sm-8">
-  	<h1><small>Sign In</small></h1>
+  	<nav class="navbar navbar-inverse">
+  
+  <p class="navbar-text">Welcome TO The Workout Calculator</p>
+</nav>
+<h2>Sign In</h2>
+  <form class="form-horizontal" id="mainForm" action="signIn.php" method="POST">
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="txtUser">Username:</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="txtUser" placeholder="Username" name="txtUser">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="txtPassword">Password:</label>
+      <div class="col-sm-10">          
+        <input type="password" class="form-control" id="txtPassword" placeholder="Password" name="txtPassword">
+      </div>
+    </div>
+    <div class="form-group">        
+      <div class="col-sm-offset-2 col-sm-10">
+        <input type="submit" class="btn btn-success" value="Sign In" />
+      </div>
+    </div>
+  </form>
 
 
-  	<form id="mainForm" action="signIn.php" method="POST">
 
-	<input type="text" id="txtUser" name="txtUser" placeholder="Username">
-	<label for="txtUser">Username</label>
-	<br /><br />
-
-	<input type="password" id="txtPassword" name="txtPassword" placeholder="Password">
-	<label for="txtPassword">Password</label>
-	<br /><br />
-
-	<input type="submit" class="btn btn-success" value="Sign In" />
-
-</form>
 <br /><br />
 
 Or <a href="signUp.php">Sign up</a> for a new account.
