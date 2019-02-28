79e7b1753f3cd93b5088586768010230d2821078
ankitadev <ankitae.dev@gmail.com>
2019-02-28 03:29:30 -0700

    test

--- web/exercisegenerator/insert_calorie.php
+++ web/exercisegenerator/insert_calorie.php
@@ -1,19 +1,19 @@
 <?php
 
+$id = htmlspecialchars($_POST['id']);
 $gender = htmlspecialchars($_POST['gender']);
 $age = htmlspecialchars($_POST['age']);
 $weight = htmlspecialchars($_POST['weight']);
 $height = htmlspecialchars($_POST['height']);
 $cal = htmlspecialchars($_POST['cal']);
 
-//echo "Trying to insert: courseid: $course_id, date: $date, content: $content";
-
 require_once("dbconnect.php");
 $db = get_db();
 
 // Get the Course from the DB
-$query = 'INSERT INTO calories(gender, age, weight, height, cal) VALUES(:gender, :age, :weight, :height, :cal)';
+$query = 'INSERT INTO calories(id, gender, age, weight, height, cal) VALUES(:id, :gender, :age, :weight, :height, :cal)';
 $statement = $db->prepare($query);
+$statement->bindValue(':id', $id, PDO::PARAM_STR);
 $statement->bindValue(':gender', $gender, PDO::PARAM_STR);
 $statement->bindValue(':age', $age, PDO::PARAM_STR);
 $statement->bindValue(':weight', $weight, PDO::PARAM_STR);
@@ -23,6 +23,7 @@ $result = $statement->execute();
 
 //echo "$result";
 flush();
+header("Location:caloriecounter.php?id=$id");
 die();
 
 ?>
