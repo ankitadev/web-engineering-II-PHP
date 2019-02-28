1567bf8082eb80a9359c15dff1f7f838a0b0f1a6
ankitadev <ankitae.dev@gmail.com>
2019-02-28 04:11:04 -0700

    test

--- web/exercisegenerator/insert_bmi.php
+++ web/exercisegenerator/insert_bmi.php
@@ -2,20 +2,19 @@
 
 $weight = htmlspecialchars($_POST['weight']);
 $height = htmlspecialchars($_POST['height']);
-$bmical = htmlspecialchars($_POST['bmical']);
 
 require_once("dbconnect.php");
 $db = get_db();
 
 // Get the Course from the DB
-$query = 'INSERT INTO calories(weight, height, bmical) VALUES(:weight, :height, :bmical)';
+$query = 'INSERT INTO calories(weight, height) VALUES(:weight, :height)';
 $statement = $db->prepare($query);
 $statement->bindValue(':weight', $weight, PDO::PARAM_STR);
 $statement->bindValue(':height', $height, PDO::PARAM_STR);
-$statement->bindValue(':bmical', $bmical, PDO::PARAM_STR);
 $result = $statement->execute();
 
 //echo "$result";
 flush();
+header("Location:caloriecounter.php?weight=$weight");
 die();
 ?>
